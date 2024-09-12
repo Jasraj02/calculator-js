d7063abd356c12905bbad99cca488d7046a970a0
Jasraj Aulakh <177463743+Jasraj02@users.noreply.github.com>
2024-09-12 19:32:19 +0100

    Create very basic calculator UI
    Basic UI of the calculator has been built using
    HTML and CSS. UI interactivity will be added next

---
+++ calculatorCode.js
@@ -0,0 +1,8 @@
+
+// functions needed to add, subtract, divide and multiple 
+
+// Next steps
+// make the operate function (which takes two numbers and a operator and returns appropriate result)
+// could add pictures to the buttons to make them look nicer 
+
+
