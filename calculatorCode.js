a98ca2a4c5f4f972eb9a4265c1395b16023c2e8c
Jasraj Aulakh <177463743+Jasraj02@users.noreply.github.com>
2024-09-12 19:50:29 +0100

    Create operations functions and operate function
    The functions for all operations have been made,
    along with the operate function which calls upon
    these functions

--- calculatorCode.js
+++ calculatorCode.js
@@ -6,3 +6,45 @@
 // could add pictures to the buttons to make them look nicer 
 
 
+
+// all operation functions 
+
+function add(a,b) {
+    return a+b
+}
+
+function subtract(a,b) {
+    return a-b
+}
+
+function multiply(a,b) {
+    return a*b
+}
+
+function divide(a,b) {
+    return a/b
+}
+
+function exponential(a,b) {
+    return a**b
+}
+
+// operate function which takes two variables and a operator
+
+function operate(numb1,numb2,symbol) {
+    if (symbol === "+") {
+        return add(numb1,numb2)
+    }
+    else if (symbol === "-") {
+        return subtract(numb1,numb2)
+    }
+    else if (symbol === "x") {
+        return multiply(numb1,numb2)
+    }
+    else if (symbol === "/") {
+        return divide(numb1,numb2)
+    }
+    else if (symbol === "^") {
+        return exponential(numb1,numb2)  
+    }
+    }
